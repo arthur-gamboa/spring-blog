6a171c43c8867422cd543d46f6cec7bb8b20242c
Arthur Gamboa <arthur.j.gamboa@gmail.com>
2021-03-27 15:59:38 -0500

    configure data source & create post repo

---
+++ src/main/java/com/codeup/codeup_demo/repo/PostRepository.java
@@ -0,0 +1,17 @@
+package com.codeup.codeup_demo.repo;
+
+import com.codeup.codeup_demo.models.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public class PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findByTitle(String titleToSearchFor);
+
+    @Query("from Post post where post.body like %:term%")
+    List<Post>searchByBodyLike(@Param("term") String term);
+
+}
