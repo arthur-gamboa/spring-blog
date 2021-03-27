793048a3b5386f8af72a33ac90475c2ed554d461
Arthur Gamboa <arthur.j.gamboa@gmail.com>
2021-03-26 22:49:39 -0500

    finished templates and building out views for posts

--- src/main/java/com/codeup/codeup_demo/controllers/PostController.java
+++ src/main/java/com/codeup/codeup_demo/controllers/PostController.java
@@ -1,24 +1,37 @@
 package com.codeup.codeup_demo.controllers;
 
+import com.codeup.codeup_demo.models.Post;
+import com.codeup.codeup_demo.repo.PostRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class PostController {
 
+    private final PostRepository postDao;
+
+    public PostController(PostRepository postDao){
+        this.postDao = postDao;
+    }
+
     @GetMapping("/posts")
-    @ResponseBody
-    public String seeAllPosts() {
-        return "All posts will display here.";
+    public String seeAllPosts(Model viewModel) {
+        List<Post> postsFromDB = postDao.searchByBodyLike("post");
+        viewModel.addAttribute("posts", postsFromDB);
+        return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String showPost(@PathVariable int id) {
-        return "You will see a post with the ID of " + id + ".";
+    public String showPost(@PathVariable int id, Model vModel) {
+        vModel.addAttribute("post", new Post("iPad", "Pro 11in"));
+        return "posts/show";
     }
 
     @GetMapping("/posts/create")
---
+++ src/main/java/com/codeup/codeup_demo/models/Post.java
@@ -0,0 +1,50 @@
+package com.codeup.codeup_demo.models;
+
+import javax.persistence.*;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@Entity
+@Table(name="posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 225, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", length = 3000, nullable = false)
+    private String body;
+
+    public Post() {}
+
+    public Post(Long id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+}
